fffb59ecff421d212114bdaf5c5a5112d49bc96c
daajt1 <daltramarket@gmail.com>
2020-12-09 18:51:39 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// importing data
+const { courses } = require('./data.json');
+
+// GrapqhQL Schema
+const schema = buildSchema(`
+  type Query {
+    course(id: Int!): Course
+    courses(topic: String): [Course]
+  }
+  type Mutation {
+    updateCourseTopic(id: Int!, topic: String!): Course
+  }
+  type Course {
+    id: Int
+    title: String
+    author: String
+    description: String
+    topic: String
+    url: String
+  }
+`);
+
+let getCourse = (args) => {
+  let id = args.id;
+  return courses.filter(course => {
+      return course.id == id;
+  })[0];
+}
+
+let getCourses = (args) => {
+  if (args.topic) {
+    let topic = args.topic;
+    return courses.filter(course => course.topic === topic);
+  } else {
+    return courses;
+  }
+}
+
+let updateCourseTopic = ({id, topic}) => {
+  courses.map(course => {
+    if (course.id === id) {
+      course.topic = topic;
+      return course;
+    }
+  });
+
+  return courses.filter(course => course.id === id)[0];
+}
+
+const root = {
+  course: getCourse,
+  courses: getCourses,
+  updateCourseTopic: updateCourseTopic
+};
+
+// GraphQL endpoint
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }));
+
+app.listen(4000);
+console.log("on port 4000");
