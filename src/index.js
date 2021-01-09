0b0089eb5c3b835caaf654e54bde0afee779a566
daajt1 <daltramarket@gmail.com>
2021-01-09 13:07:02 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,69 +1,26 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
-
-// importing data
-const { courses } = require('./data.json');
-
-// GrapqhQL Schema
-const schema = buildSchema(`
+ 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
   type Query {
-    course(id: Int!): Course
-    courses(topic: String): [Course]
-  }
-  type Mutation {
-    updateCourseTopic(id: Int!, topic: String!): Course
-  }
-  type Course {
-    id: Int
-    title: String
-    author: String
-    description: String
-    topic: String
-    url: String
+    hello: String
   }
 `);
-
-let getCourse = (args) => {
-  let id = args.id;
-  return courses.filter(course => {
-      return course.id == id;
-  })[0];
-}
-
-let getCourses = (args) => {
-  if (args.topic) {
-    let topic = args.topic;
-    return courses.filter(course => course.topic === topic);
-  } else {
-    return courses;
-  }
-}
-
-let updateCourseTopic = ({id, topic}) => {
-  courses.map(course => {
-    if (course.id === id) {
-      course.topic = topic;
-      return course;
-    }
-  });
-
-  return courses.filter(course => course.id === id)[0];
-}
-
-const root = {
-  course: getCourse,
-  courses: getCourses,
-  updateCourseTopic: updateCourseTopic
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
 };
-
-// GraphQL endpoint
+ 
 var app = express();
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  }));
-
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 app.listen(4000);
-console.log("on port 4000");
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
